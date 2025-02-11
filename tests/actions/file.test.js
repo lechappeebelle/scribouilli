57a18abfde88eea2c05e3a0e0dbf715323e48c54
Yannick François <yaf@elsif.fr>
2025-02-11 11:14:22 +0100

    :recycle: clean file tests

--- tests/actions/file.test.js
+++ tests/actions/file.test.js
@@ -22,96 +22,98 @@ describe('actions/file.js', () => {
   })
 
   describe('writeFileAndCommit', () => {
-    before(() => {
-      sandbox.stub(gitAgent, 'writeFile').resolves()
-      sandbox.stub(gitAgent, 'commit').resolves()
-    })
-
     it('calls writeFile and commit', done => {
-      writeFileAndCommit('test.js', 'Curiouser and curiouser!')
-        .then(() => {
-          expect(gitAgent.writeFile).to.have.been.calledWith(
-            store.state.currentRepository,
-            'test.js',
-            'Curiouser and curiouser!',
-          )
-          expect(gitAgent.commit).to.have.been.calledWith(
-            store.state.currentRepository,
-            'Modification du fichier test.js',
-          )
-          done()
-        })
-        .catch(e => done(e))
+      const fakeStore = {
+        state: {
+          gitAgent: {
+            writeFile: sinon.stub(),
+            commit: sinon.stub(),
+          },
+        },
+      }
+      writeFileAndCommit(
+        'test.js',
+        'Curiouser and curiouser!',
+        undefined,
+        fakeStore,
+      )
+      done()
+      expect(fakeStore.state.gitAgent.writeFile).to.have.been.calledWith(
+        'bla',
+        'test.js',
+        'Curiouser and curiouser!',
+      )
+      expect(faceStore.state.gitAgent.commit).to.have.been.calledWith(
+        'fanne',
+        'Modification du fichier test.js',
+      )
     })
   })
 
   describe('writeFileAndPushChanges', () => {
-    before(() => {
-      sandbox.stub(gitAgent, 'writeFile').resolves()
-      sandbox.stub(gitAgent, 'commit').resolves()
-      sandbox.stub(gitAgent, 'safePush').resolves()
-    })
-
     it('calls writeFile, commit and push', done => {
-      writeFileAndPushChanges('test.js', 'Curiouser and curiouser!')
-        .then(() => {
-          expect(gitAgent.writeFile).to.have.been.calledWith(
-            store.state.currentRepository,
-            'test.js',
-            'Curiouser and curiouser!',
-          )
-          expect(gitAgent.commit).to.have.been.calledWith(
-            store.state.currentRepository,
-            `Modification du fichier test.js`,
-          )
-          expect(gitAgent.safePush).to.have.been.calledWith(
-            store.state.currentRepository,
-          )
-          done()
-        })
-        .catch(e => done(e))
+      const fakeStore = {
+        state: {
+          gitAgent: {
+            removeFile: sinon.stub(),
+            commit: sinon.stub(),
+          },
+        },
+      }
+      writeFileAndPushChanges(
+        'test.js',
+        'Curiouser and curiouser!',
+        undefined,
+        fakeStore,
+      )
+      done()
+      expect(gitAgent.writeFile).to.have.been.calledWith(
+        store.state.currentRepository,
+        'test.js',
+        'Curiouser and curiouser!',
+      )
+      expect(gitAgent.commit).to.have.been.calledWith(
+        store.state.currentRepository,
+        `Modification du fichier test.js`,
+      )
+      expect(gitAgent.safePush).to.have.been.calledWith(
+        store.state.currentRepository,
+      )
     })
   })
 
   describe('deleteFileAndCommit', () => {
-    before(() => {
-      sandbox.stub(gitAgent, 'removeFile').resolves()
-      sandbox.stub(gitAgent, 'commit').resolves()
-    })
-
     it('calls removeFile and commit', done => {
-      deleteFileAndCommit('test.js')
-        .then(() => {
-          expect(gitAgent.removeFile).to.have.been.calledWith(
-            store.state.currentRepository,
-            'test.js',
-          )
-          expect(gitAgent.commit).to.have.been.calledWith(
-            store.state.currentRepository,
-            'Suppression du fichier test.js',
-          )
-          done()
-        })
-        .catch(e => done(e))
+      const fakeStore = {
+        state: {
+          gitAgent: {
+            removeFile: sinon.stub(),
+            commit: sinon.stub(),
+          },
+        },
+      }
+
+      deleteFileAndCommit('test.js', undefined, fakeStore)
+      done()
+      expect(fakeStore.state.gitAgent.removeFile).to.have.been.calledOnce
+      expect(fakeStore.state.gitAgent.commit).to.have.been.calledOnce
     })
   })
 
   describe('deleteFileAndPushChanges', () => {
-    before(() => {
-      sandbox.stub(gitAgent, 'removeFile').resolves()
-      sandbox.stub(gitAgent, 'commit').resolves()
-      sandbox.stub(gitAgent, 'safePush').resolves()
-    })
-
     it('calls removeFile and commit', done => {
-      deleteFileAndPushChanges('test.js')
-        .then(() => {
-          expect(gitAgent.safePush).to.have.been.calledWith(
-            store.state.currentRepository,
-          )
-          done()
-        })
-        .catch(e => done(e))
+      const fakeStore = {
+        state: {
+          gitAgent: {
+            removeFile: sinon.stub(),
+            safePush: sinon.stub(),
+            commit: sinon.stub(),
+          },
+        },
+      }
+      deleteFileAndPushChanges('test.js', undefined, fakeStore)
+      expect(fakeStore.state.gitAgent.safePush).to.have.been.calledOnce
+      done()
     })
   })
 })
