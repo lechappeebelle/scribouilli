75aeb43ed17242baf7a244fc33cb27f7093adaf5
Fanny Cheung <dev@ynote.hk>
2025-02-12 14:13:51 +0100

    Sortir les types dans des fichiers différents

--- assets/scripts/types/git.js
+++ assets/scripts/types/git.js
@@ -1,21 +1,4 @@
 /**
- * @typedef {Object} Page
- * @property {string} path
- * @property {string} title
- * @property {string} [content]
- * @property {boolean} [inMenu]
- * @property {boolean} [blogIndex] Is this page the index of the blog section? Defaults to false.
- * @property {number} index
- */
-
-/**
- * @typedef {Object} Article
- * @property {string} path
- * @property {string} title
- * @property {string} [content]
- */
-
-/**
  * @typedef {Object} ScribouilliGitRepo
  * @property {string} repoId
  * @property {string} owner
@@ -33,6 +16,7 @@
  */
 
 /**
+ * @description On pense que ça correspond au build status de GitHub?
  * @typedef {"in_progress" | "success" | "error" | "not_public"} BuildStatus
  */
 
@@ -82,25 +66,4 @@
  * @property {string} name
  * @property {Object} owner
  * @property {string} owner.login
- */
-
-/**
- * @typedef {Object} EditeurFile
- * @property {string} fileName
- * @property {string} content
- * @property {string | undefined} previousContent
- * @property {string} title
- * @property {number} index
- * @property {string | undefined} previousTitle
- * @property {boolean} blogIndex
- */
-
-/**
- * @typedef {Object} FileContenu
- * @property {string} path
- * @property {string} title
- * @property {string} content
- * @property {number} index
- * @property {boolean} inMenu
- * @property {boolean} blogIndex
  */
