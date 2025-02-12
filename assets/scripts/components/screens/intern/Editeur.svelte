dc08bcb96a0f99c1a1b700574bd9d891f99f1a5d
maiwann <smogo@maiwann.net>
2025-02-12 15:14:29 +0100

    lien dans l'éditeur pour encart/bouton

--- assets/scripts/components/screens/intern/Editeur.svelte
+++ assets/scripts/components/screens/intern/Editeur.svelte
@@ -174,12 +174,19 @@
                   >
                     avec du Markdown
                   </a>
-                  … ou
+                  … ou avec du HTML grâce 
+                  <a
+                    href="https://scribouilli.org/aide.html"
+                    target="_blank"
+                  >
+                    à nos exemples
+                  </a>
+                  d'encart ou de bouton, ou 
                   <a
                     href="https://developer.mozilla.org/fr/docs/Learn_web_development/Core/Structuring_content"
                     target="_blank"
                   >
-                    apprendre le HTML
+                    avec ce guide.
                   </a>
                 </p>
               </div>
